189f257689510e94557f6da74698dcac85607138
kk <kt7641725@gmail.com>
2026-02-13 11:54:06 +0530

    first commit

--- newfolder/crash.js
+++ newfolder/crash.js
@@ -1,3 +1,34 @@
-const crash =()=>{
-    
-}
+
+// login()
+// .then(getusers)
+// .then(console.log)
+
+const { response } = require("express");
+
+ Promise.resolve(10)
+ .then((x)=>{
+    return x+10;
+ })
+ .then((x)=>{
+    console.log(x);
+    return x+20;
+ })
+ .then(console.log);
+
+
+ const fetchData=async()=>{
+    console.log("started");
+    await fetch()
+    .then((response)=>
+        response.json()
+    )
+    .then((json)=>
+        response.json()
+    )
+    console.log("Ended");
+ }
+
+ console.log("Started------1")
+
+ fetchData();
+  console.log("Started------2")
